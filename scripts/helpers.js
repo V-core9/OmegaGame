c25fec11cb06bf14f20d541f6fba418612906a15
Slavko V <slavko.vuletic92@gmail.com>
2020-12-17 21:17:09 +0100

    build: version 2 initial files setup

---
+++ scripts/helpers.js
@@ -0,0 +1,25 @@
+/*
+
+| File: helpers.js
+: Description: Contains general helpers functions for the both game and application.
+@ Author: Slavko V.
+
+! Note: Insert into html before app.js 
+
+? Requires: [-none-]
+
+*/
+
+// 1. Loading InterScreen Functions
+    // 1.1. Show Loading Screen 
+    function showLoadInterScreen(){
+
+    }
+    // !end! 1.1. Show Loading Screen
+
+    // 1.2. Hide Loading Screen 
+    function hideLoadInterScreen(){
+
+    }
+    // !end! 1.2. Hide Loading Screen
+// !end! 1. Loading InterScreen Functions
