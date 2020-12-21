276fba1b98bc2b73e29bde518877c03e39a136f0
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2020-12-21 06:06:14 +0100

    update build and serve

--- OmegaBuilder/server.express.sh
+++ OmegaBuilder/server.express.sh
@@ -4,8 +4,6 @@ start omega.build.sh
 
 echo "Serving files using Node-Express.js at http://localhost:5000"
 
-start "http://localhost:5000"
-
 cd ..
 
-node server.express.js
+start "http://localhost:5000" && node server.express.js
