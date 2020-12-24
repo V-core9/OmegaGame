01a5c510d1eb75613eced9eadcfd601bf10b41f5
Slavko V <slavko.vuletic92@gmail.com>
2020-12-24 21:24:25 +0100

    build: SCSS support added through node-sass -g, normal for dev & minified in production

--- OmegaBuilder/omega.build.dev.sh
+++ OmegaBuilder/omega.build.dev.sh
@@ -1,11 +1,27 @@
 echo "Starting Omega Build."
 
+echo "Trying to remove public folder if exists"
+rm -r ../public
+read -p "To continue with OmegaBuild press: [enter]"
+
 echo "Make Public dir"
 mkdir ../public
 echo "Done Public dir."
+read -p "To continue with OmegaBuild press: [enter]"
 
 echo "Copy Source Files to public folder"
 cp -r ../src/* ../public 
 echo "Done Copy Source Files."
+read -p "To continue with OmegaBuild press: [enter]"
 
-#read -p "To continue with OmegaInstall press: [enter]"
+echo "Remove All Css Files From PublicBuild"
+rm -r ../public/**/*.scss
+echo "Done Removing CSS files"
+read -p "To continue with OmegaBuild press: [enter]"
+
+echo "Now Exporting all SCSS files from src to public"
+node-sass -r ../src/ -o ../public/
+echo "Done Exporting CSS files"
+read -p "To continue with OmegaBuild press: [enter]"
+
+read -p "To EXIT OmegaBuild press: [enter]"
