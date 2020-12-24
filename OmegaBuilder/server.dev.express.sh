93f539916077f15be3a726769d9d99763c2a6864
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2020-12-24 10:23:03 +0100

    more adding and change to express server to run SPA app mode

--- OmegaBuilder/server.dev.express.sh
+++ OmegaBuilder/server.dev.express.sh
@@ -7,3 +7,5 @@ echo "Serving files using Node-Express.js at http://localhost:5000"
 cd ..
 
 start "http://localhost:5000" && node server.express.js
+
+#read -p "All Done! To exit install press: [enter]"
---
+++ src/logic/app.js
@@ -0,0 +1,28 @@
+//  Main Application File
+//
+//  File Name: app.js
+//  Description: Main application file that will handle everything around the actuall game.
+//  Requires: helpers.js, app.config.js
+//  Author: Slavko V.
+//  Url: #
+//
+//----------------------------------------------------
+//
+//  TABLE OF CONTENT:
+//  1. demoANme() -> some short description 
+//  2. demoANme() -> some short description 
+//  3. demoANme() -> some short description 
+//>     1. demoANme() -> some short description 
+//>     2. demoANme() -> some short description 
+//  4. demoANme() -> some short description 
+//  5. demoANme() -> some short description 
+//>     1. demoANme() -> some short description 
+//>     2. demoANme() -> some short description 
+//
+//
+//
+//
+//====================================================
+
+
+alert('yeaa');
