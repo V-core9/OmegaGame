27d4ac9fc73f07840c4a866b551dee3970f1728a
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2020-12-24 11:48:20 +0100

    yeaaa more

--- src/logic/helpers.js
+++ src/logic/helpers.js
@@ -1,2 +1,29 @@
-// File Containing General Helper Functions
+//  Main Application File
+//
+//  File Name: app.js
+//  Description: Main application file that will handle everything around the actuall game.
+//  Requires: helpers.js, app.config.js
+//  Author: Slavko V.
+//  Url: #
+//
+//----------------------------------------------------
+//
+//  TABLE OF CONTENT:
+//  1. detectCurrentDevice() -> Detects current device and fills into mainApp.device
+//  2. findCurrentPage() -> Detects current page and sets in mainApp
+//  3. openCurrentPage() -> Handles opening of the current page 
+//  4. loadJSfile() -> Helper that handles loading of JS files 
+//  5. loadCSSfile() -> Helper that handles loading of CSS files
+//  6. createCookie() -> Makes new cookie
+//  7. getCookie() -> Gets the value of cookie
+//  8. humanTime() -> Readable time format from default [either formats value or returns current formated]
+//
+//====================================================
 
+
+
+//  1. detectCurrentDevice()
+function detectCurrentDevice(){
+    console.log('detectCurrentDevice');
+}
+//!  1. detectCurrentDevice() !!
