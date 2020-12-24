5f67fc12beaa5f148ce42b2861c4d94b80185d92
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2020-12-24 11:48:20 +0100

    yeaaa more

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,7 +1,35 @@
-// Main Application Configuration File 
-var app = {
+// Main Application Configuration/Variables File 
+//
+//  File Name: [src/config/] app.config.js
+//  Description: 
+//      Well...many variables in charge of running the app. Some of those for example would be application name,
+//      app status [like loading], current page...etc. Also parts will be added as app loads and figures out stuff
+//      like device, screen size, page hystory and more.
+//  Requires: [---]
+//  Author: Slavko V.
+//  Url: #
+//
+//----------------------------------------------------
+//
+//  TABLE OF CONTENT:
+//  1. mainApp -> main app variable[object]
+//>     1. name -> Application Name
+//>     2. status -> Current App Status
+//>     3. page -> Currently Open [to-open] page
+//>     4. bench -> [ShortBurstTimedBenchmark] Short device benchmark results before setting up video options
+//
+//
+//
+//
+//====================================================
+
+
+
+
+
+var mainApp = {
     name : "OmegaGame",
     status : "init",
-    currentPage : "",
-    sbtBench: "",
+    page : "",
+    bench: 0,
 }
