35f7f7fefd74f7318287ea2e1e0c26a056d24f9d
Slavko_V <slavko.vuletic92@gmail.com>
2021-01-22 12:16:32 +0100

    fix for babel minify to take from src folder....so imgs dont get affected

--- OmegaBuilder/omega.build.sh
+++ OmegaBuilder/omega.build.sh
@@ -42,18 +42,22 @@ else
     rm -r public/**/*.scss
     echo "Done Removing SCSS files"
 
+    #read -p "[next: remove all SCSS from public] >> press [enter] to continue;"
+    rm -r public/**/*.js
+    echo "Done Removing SCSS files"
+
     #read -p "[next: export all SCSS from src->public] >> press [enter] to continue;"
     node-sass -r src/ -o public/ 
     echo "Done Exporting CSS files"
 
     #read -p "Now Exporting JS files using BABEL [with remove-comments plugin]"
-    npx babel src/ --out-dir public/ --source-maps --plugins remove-comments
+    npx babel src/ --out-dir public/ --source-maps --plugins remove-comments --ignore "src/assets/**/*"
     echo "Done Exporting BABEL JS files [ part_1 ]"
 
-    find public/* -type f -exec sed -i '/PATTERN-1/,/PATTERN-2/d' {} +
+    #find public/* -type f -exec sed -i '/PATTERN-1/,/PATTERN-2/d' {} +
 
     #read -p "Now Exporting JS files using BABEL [MINIFY separate run]"
-    minify public/ --out-dir public/ --mangle.keepClassName
+    minify src/ --out-dir public/ --mangle.keepClassName
     echo "Done Exporting BABEL JS files [ part_2 ]"
 
     #read -p "To EXIT OmegaBuild press: [enter]"
