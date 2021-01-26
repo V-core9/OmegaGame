4207a5a11fead44a09337de1c608755de6672c39
Slavko_V <slavko.vuletic92@gmail.com>
2021-01-26 14:27:07 +0100

    docs fix

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,97 @@
-console.log("App STARTING file..>.>.>.>");var leftSvg,centerSvg,rightSvg,animConfig={randomColors:!1};(function(){window.addEventListener("resize",startAnimationFunc),startAnimationFunc()})();function startAnimationFunc(){leftSvg=document.getElementById("leftSvg"),centerSvg=document.getElementById("centerSvg"),rightSvg=document.getElementById("rightSvg"),redrawThreeElems()}function redrawThreeElems(){getSizes(),leftSvg.setAttribute("width",animConfig.screenSingleWidthHelper),centerSvg.setAttribute("width",animConfig.screenSingleWidthHelper),rightSvg.setAttribute("width",animConfig.screenSingleWidthHelper),leftSvg.setAttribute("height",animConfig.animHeight),centerSvg.setAttribute("height",animConfig.animHeight),rightSvg.setAttribute("height",animConfig.animHeight);var a=.01*animConfig.screenSingleWidthHelper+","+.005*animConfig.screenSingleWidthHelper;a+=" "+.99*animConfig.screenSingleWidthHelper+","+.15*animConfig.animHeight,a+=" "+.99*animConfig.screenSingleWidthHelper+","+.89*animConfig.animHeight,a+=" "+.01*animConfig.screenSingleWidthHelper+","+.74*animConfig.animHeight,leftSvg.querySelector("polygon").setAttribute("points",a);var b=.01*animConfig.screenSingleWidthHelper+","+.005*animConfig.screenSingleWidthHelper;b+=" "+animConfig.screenSingleWidthHelper/2+","+.075*animConfig.animHeight,b+=" "+.99*animConfig.screenSingleWidthHelper+","+.005*animConfig.screenSingleWidthHelper,b+=" "+.99*animConfig.screenSingleWidthHelper+","+.9*animConfig.animHeight,b+=" "+animConfig.screenSingleWidthHelper/2+","+.99*animConfig.animHeight,b+=" "+.01*animConfig.screenSingleWidthHelper+","+.9*animConfig.animHeight,centerSvg.querySelector("polygon").setAttribute("points",b);var c=.01*animConfig.screenSingleWidthHelper+","+.15*animConfig.animHeight;c+=" "+.99*animConfig.screenSingleWidthHelper+","+.005*animConfig.screenSingleWidthHelper,c+=" "+.99*animConfig.screenSingleWidthHelper+","+.74*animConfig.animHeight,c+=" "+.01*animConfig.screenSingleWidthHelper+","+.89*animConfig.animHeight,rightSvg.querySelector("polygon").setAttribute("points",c)}function getSizes(){animConfig.screenSingleWidthHelper=window.innerWidth/3,animConfig.animHeight=window.innerHeight}function loadDoc(){var a=new XMLHttpRequest;a.onreadystatechange=function(){4==this.readyState&&200==this.status&&console.log(this.responseText)},a.open("GET","package.json",!0),a.send()}loadDoc();
+//╔═══════════════════════════════════════════════════════════════════════════════╗
+//║ ♠ File Name: app.js                                                           ║
+//║ ↔ Location: <: public_root :>/ app.js                                         ║
+//║ Σ Description:                                                                ║
+//║     → In case this lives more than few days, better have something prepared   ║
+//╟       than few days, better have something prepared have as   ╔═══════════════╣
+//║       than few days, better have something prepared.          ║ √ 23.01.2021. ║
+//╚═══════════════════════════════════════════════════════════════╩═══════════════╝
+
+console.log('App STARTING file..>.>.>.>')
+
+var animConfig = {
+    randomColors: false,
+}
+
+var leftSvg, centerSvg, rightSvg;
+
+
+function startAnimationFunc() {
+    leftSvg = document.getElementById('leftSvg');
+    centerSvg = document.getElementById('centerSvg');
+    rightSvg = document.getElementById('rightSvg');
+    redrawThreeElems();
+}
+
+
+// Draws 3 "banners"
+function redrawThreeElems() {
+
+    getSizes();
+
+    leftSvg.setAttribute("width", animConfig.screenSingleWidthHelper);
+    centerSvg.setAttribute("width", animConfig.screenSingleWidthHelper);
+    rightSvg.setAttribute("width", animConfig.screenSingleWidthHelper);
+    leftSvg.setAttribute("height", animConfig.animHeight);
+    centerSvg.setAttribute("height", animConfig.animHeight);
+    rightSvg.setAttribute("height", animConfig.animHeight);
+
+    var leftSvgPoints = (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.screenSingleWidthHelper * 0.005);
+    leftSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.animHeight * 0.15);
+    leftSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.animHeight * 0.89);
+    leftSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.animHeight * 0.74);
+
+    leftSvg.querySelector('polygon').setAttribute("points", leftSvgPoints);
+
+    var centerSvgPoints = (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.screenSingleWidthHelper * 0.005);
+    centerSvgPoints += " " + (animConfig.screenSingleWidthHelper / 2) + "," + (animConfig.animHeight * 0.075);
+    centerSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.screenSingleWidthHelper * 0.005);
+    centerSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.animHeight * 0.9);
+    centerSvgPoints += " " + (animConfig.screenSingleWidthHelper / 2) + "," + (animConfig.animHeight * 0.99);
+    centerSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.animHeight * 0.9);
+
+    centerSvg.querySelector('polygon').setAttribute("points", centerSvgPoints);
+
+    var rightSvgPoints = (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.animHeight * 0.15);
+    rightSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.screenSingleWidthHelper * 0.005);
+    rightSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.99) + "," + (animConfig.animHeight * 0.74);
+    rightSvgPoints += " " + (animConfig.screenSingleWidthHelper * 0.01) + "," + (animConfig.animHeight * 0.89);
+
+    rightSvg.querySelector('polygon').setAttribute("points", rightSvgPoints);
+
+}
+// End (3 elems) banners
+
+// Gets sizes and puts into animConfig
+function getSizes() {
+    animConfig.screenSingleWidthHelper = window.innerWidth / 3;
+    animConfig.animHeight = window.innerHeight;
+}
+// End getting sizes
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //document.getElementById("demo").innerHTML = this.responseText;
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "package.json", true);
+    xhttp.send();
+}
+
+(function () {
+    window.addEventListener("resize", startAnimationFunc);
+
+    startAnimationFunc();
+})();
+
+loadDoc();
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+//▌   ! END OF FILE !            ▐▀▀▀         ▐▀▀▀             ! END OF FILE !    ▐
+//▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄      ▐▀▀▀  ▐▀▀▀▀▌ ▐▀▀▀         ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐
+//▌ ♠ File Name: app.js   ▐      ▐▄▄▄  ▐▄▄▄▄▌ ▐            ▌  ☺ Author: Slavko V. ▐
+//▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
