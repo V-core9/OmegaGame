4dc5597a5086a778773d7f4cd83a705beeb3f6e3
Slavko_V <slavko.vuletic92@gmail.com>
2021-04-21 17:53:30 +0200

    not sure what am I submitting

--- src/BloodyArena/js/app.js
+++ src/BloodyArena/js/app.js
@@ -12,13 +12,30 @@ var map = {
 var orchestratorWW = new Worker('ww/orchestrator.js');
 
 orchestratorWW.onmessage = function(e) {
-    console.log('Message received from worker '+e.data);
+    //console.log(e.data);
+    switch (e.data) {
+        case "updateGame":
+            console.log('updatingGameSpace....>>')
+            break;
+    
+        case "updateScreen":
+            console.log('updatingScreen....>>')
+            break;
+    
+        case "updateInscreen":
+            console.log('updatingINScreenER....>>')
+            break;
+    
+        default:
+            break;
+    }
 }
 
 var app = {
     cnv: document.getElementById('appCam'),
     cnvW: window.innerWidth,
     cnvH: window.innerWidth*9/16,
+    camStyle: 'attached',
     start: function(){
         console.log('app.start()[S]');
         this.cnv.width = this.cnvW;
